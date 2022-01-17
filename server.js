8d93f8e6f0343d84699c7425ad7a01b48858f77f
KRD13 <rauchdavis13@gmail.com>
2022-01-17 14:17:28 -0500

    added routes

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// test route
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello world'
+//   });
+// });
+
+// default repsonse for any oher request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
