f1d1163970895c1c10d81a80d0221d2ea03090c6
KRD13 <rauchdavis13@gmail.com>
2022-01-17 14:55:22 -0500

    setup MySQL and Express

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const database = require('mime-db');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // your MySQL username
+    user: 'root',
+    // your MySQL password
+    password: 'Athame@13',
+    database: 'election'
+  },
+  console.log('Connected to the election datatbase.')
+);
 
 // test route
 // app.get('/', (req, res) => {
